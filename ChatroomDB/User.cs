27c113e779c2ae3cc925cc3ee72d455d33a4854b
unknown <philip.frauscher@gmail.com>
2017-11-09 15:55:20 +0100

    Database Work

---
+++ ChatroomDB/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatroomDB
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public bool isOnline { get; set; }
+        public string UserEmail { get; set; }
+        public bool isAdmin { get; set; }
+        public string UserTag { get; set; }
+        public virtual Chatroom UserChatroom { get; set; }
+
+    }
+}
